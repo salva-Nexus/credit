5c39cc4dfd246bf3f555c886476b8a70242ef94c
salvanexus <charlieonyii@gmail.com>
2026-04-23 12:42:13 +0100

    New

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -228,7 +228,7 @@ router.post("/login", async (req, res) => {
       const token = jwt.sign(
         { id: user._id, role: user.role },
         process.env.JWT_SECRET,
-        { expiresIn: "7d" },
+        { expiresIn: "12h" },
       );
       return res.json({
         token,
